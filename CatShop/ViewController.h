31e7af88233ddeb32a52414305e8fe778a79bafd
VoidLess <mail@voidless.ru>
2012-03-21 18:16:45 +0400

    Initial Commit

---
+++ CatShop/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CatShop
+//
+//  Created by Bersenev Ilya on 3/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
